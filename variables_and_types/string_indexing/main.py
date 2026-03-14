e341947cc11b8138090f9c673d4adfbb0e477bba
jonasstrbel-cmd <>
2026-03-14 11:59:45 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,12 @@
+grocery_item = "Grilled Chicken Salad"
+print(len(grocery_item))
+
+length_of_item = len(grocery_item)
+
+first_char  = grocery_item[0]
+second_char = grocery_item[8]
+third_char  = grocery_item[16]
+
+last_char1 = grocery_item[-1]   
+last_char2 = grocery_item[-7]   
+last_char3 = grocery_item[-15]  
