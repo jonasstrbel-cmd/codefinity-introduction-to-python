c7b89e4c2ea97464e955725294e397a3782de934
jonasstrbel-cmd <>
2026-03-15 12:54:14 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,11 @@
+
+item_name = "Bread"
+item_price = 4.52
+items_in_stock = 230
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
+
+print(item_name,item_price,items_in_stock)
